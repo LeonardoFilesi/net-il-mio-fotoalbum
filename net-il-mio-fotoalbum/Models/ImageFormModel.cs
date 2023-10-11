ac32c10e9fb70f7ab2e7d8a26514983d7092551b
LeonardoFilesi <leonardo.filesi@gmail.com>
2023-10-11 12:00:54 +0200

    fatta migration, aggiornato db, aggiunto ImageFormModel per relazioni N:N

--- net-il-mio-fotoalbum/Models/ImageFormModel.cs
+++ net-il-mio-fotoalbum/Models/ImageFormModel.cs
@@ -1,6 +1,13 @@
-﻿namespace net_il_mio_fotoalbum.Models
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace net_il_mio_fotoalbum.Models
 {
     public class ImageFormModel
     {
+        public Image Image { get; set; }
+
+        // NUOVE PROPRIETA' serve per gestire "Select" che seleziona istanze multiple nelle viste (Multiple per la relazione N a N)
+        public List<SelectListItem>? Category { get; set; }
+        public List<string>? SelectedCategoryiId { get; set; }
     }
 }
